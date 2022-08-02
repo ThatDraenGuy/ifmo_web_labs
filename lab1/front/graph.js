be25565cde9d7a24cc9a1cdde6f51c9c7079e883
Draen <haykin.oleg@rambler.ru>
2022-08-02 14:18:22 +0300

    some cleanup

--- lab1/front/graph.js
+++ lab1/front/graph.js
@@ -9,10 +9,10 @@ quadrantsData.set("11", []);
 quadrantsData.set("-11", []);
 quadrantsData.set("-1-1", []);
 quadrantsData.set("1-1", []);
-var shootingPoint = {x: 0, y: 0};
+var shootingPoint = {x: null, y: null};
 
-var labels = [];
-var max = 1;
+var range = [];
+var max = 0;
 var min = -max;
 
 const decoder = new Map();
@@ -24,7 +24,6 @@ decoder.set("1-1", {x:1,y:-1});
 var chart  = new Chart("graph", {
     type: "scatter",
     data: {
-        // labels: labels,
         datasets: [{
             fill: true,
             pointRadius: 1,
@@ -69,37 +68,32 @@ var chart  = new Chart("graph", {
 generate(null,null);
 
 function generate(x,y, r = 1) {
-    max = r;
-    min = -max;
-    labels = generateLabels(x,r);
-    quadrantsData.forEach((array, code) => {
-        array.length=0;
-        array.push(...generateGraph(code));
-    });
+    if (r!=max) {
+        max = r;
+        min = -max;
+        range = generateRange(x,r);
+        quadrantsData.forEach((array, code) => {
+            array.length=0;
+            array.push(...generateGraph(code));
+        });
+    }
     shootingPoint.x = x;
     shootingPoint.y = y;
     chart.update();
 }
 function generateData(value, i1, i2) {
-    const step = max/8;
+    const step = i2/16;
     var array = [];
-    for (let i = min; i<i1; i+= step) {
-        array.push(null);
-    }
     for (let x = i1; x <= i2; x += step) {
     array.push(eval(value));
     };
-    for (let i = i2; i<max; i+= step) {
-        array.push(null);
-    }
     return array;
 }
 
 function generateLine(value, i1, i2) {
     var array = [];
-    labels.forEach(x => {
-        if (x < i1 || x > i2) {
-        }  else {
+    range.forEach(x => {
+        if (x>=i1 && x<=i2) {
             const y = eval(value);
             array.push({x, y});
         }
@@ -114,13 +108,8 @@ function generateGraph(code) {
     const func = quadrantFunction(signX, signY);
     return generateLine(func, (signX>0 ? 0 : min), (signX>0 ? max : 0));
 }
-function generateLabels(x,r) {
-    var array = generateData("x",min,max);
-    // if (x!=null && !array.includes(x)) {
-    //     array.push(x);
-    //     array.sort((a,b) => a-b);
-    // }
-    return array;
+function generateRange(x,r) {
+    return generateData("x",min,max);
 }
 
 function getTriangleQuadrant(x, y) {
