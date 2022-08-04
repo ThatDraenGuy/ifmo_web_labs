261c3ca6bf5f82c3cc447f54b303ddee26ae8a22
Draen <haykin.oleg@rambler.ru>
2022-08-04 13:32:01 +0300

    shooting table generated by javascript instead of php

--- lab1/front/connector.js
+++ lab1/front/connector.js
@@ -1,9 +1,18 @@
 import { graphVariants, getQuadrantByName } from "./graph.js";
+
 const quadrantsKey = "quadrants";
-const constraintsKey = "constraints";
 const type = "type";
 const x_sign = "x_sign";
 const y_sign = "y_sign";
+
+const constraintsKey = "constraints";
+const constraints = new Map();
+const options = "options";
+const range = "range";
+const rangeMin = "range_min";
+const rangeMax = "range_max";
+constraints.set(options, handleOptions);
+constraints.set(range, handleRange);
 
 var oReq = new XMLHttpRequest();
 oReq.onload = function() {
@@ -26,4 +35,42 @@ function handleResponse(responseText) {
         });
         console.log(graphVariants)
     }
+    if (constraintsKey in obj) {
+        console.log(obj[constraintsKey]);
+        let constraintsReq = obj[constraintsKey];
+        Object.keys(constraintsReq).forEach(key => {
+            let element = constraintsReq[key];
+            let func = constraints.get(element[type]);
+            handleVariant(func, key, element);
+        });
+        
+    }
+}
+
+function handleVariant(func, name, data) {
+    let res = '<tr><td>' + name.toUpperCase()+":\n";
+    res += func(name, data);
+    res += '<br><span class="input-message" name="' + name + '" id="message" style="visibility: hidden;">message</span></td></tr>';
+    insertVariant(res);
+}
+
+
+function handleOptions(name, data) {
+    let optionsArray = data[options];
+    let res = "";
+    optionsArray.forEach(element => {
+        res +=' ' + element + '<input type="radio" name="' + name + '" value="' + element + '" class="radio-input"  oninput="paramChanged(name)">\n'
+    });
+    return res;
+}
+
+function handleRange(name, data) {
+    let min = data[rangeMin];
+    let max = data[rangeMax];
+    return '<input type="text" name="' + name + '" class="text-input" data-min="' + min + '"; data-max="' + max + '"; oninput="paramChanged(name)">\n';
+}
+
+function insertVariant(text) {
+    const place = document.querySelector('#shoot-table');
+    place.insertAdjacentHTML('beforeend', text);
 }
