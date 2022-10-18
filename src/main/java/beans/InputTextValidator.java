7f9fb0555e68d328e110e94377c2ff9935276c7d
Draen <haykin.oleg@rambler.ru>
2022-10-18 18:36:42 +0300

    a little cleanup

--- src/main/java/beans/InputTextValidator.java
+++ src/main/java/beans/InputTextValidator.java
@@ -1,20 +1,17 @@
 package beans;
 
-import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.FacesValidator;
 import jakarta.faces.validator.Validator;
 import jakarta.faces.validator.ValidatorException;
-import jakarta.inject.Named;
 
-import java.io.Serializable;
 import java.util.Map;
-@Named("test")
-@SessionScoped
+
+
 @FacesValidator(value="inputTextValidator")
-public class InputTextValidator implements Validator<String>, Serializable {
+public class InputTextValidator implements Validator<String> {
     @Override
     public void validate(FacesContext context, UIComponent component, String value) throws ValidatorException {
         Map<String,Object> attributes = component.getAttributes();
