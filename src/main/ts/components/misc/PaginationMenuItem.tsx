5513be36eda512f9a8c4b8068785284cfebe682f
Draen <haykin.oleg@rambler.ru>
2022-12-21 14:35:21 +0300

    pagination menu fix

--- src/main/ts/components/misc/PaginationMenuItem.tsx
+++ src/main/ts/components/misc/PaginationMenuItem.tsx
@@ -27,92 +27,104 @@ export const PaginationMenuItem: FC<PaginationMenuProps> = ({itemsCount, itemsPe
     const onNextClick = () => changePage(currentPage + 1);
     const onFirstClick = () => changePage(1);
     const onLastClick = () => changePage(pageAmount);
-    const onEllipsisClick = (left: number, right: number) => changePage((left + right) / 2);
+    // const onEllipsisClick = (left: number, right: number) => changePage((left + right) / 2);
+    //
+    // let isPageOutOfRange: boolean = false;
+    // let leftEllipsisNumber: number = 1;
+    // const pages = [...new Array(pageAmount)].map((_, i) => {
+    //     const pageNum = i+1;
+    //     const isNearCurrent = Math.abs(pageNum - currentPage) <= 1;
+    //     const isNextNearCurrent = Math.abs(pageNum + 1 - currentPage) <= 1;
+    //     const isNextLast = isLast(pageNum+1);
+    //
+    //     if (isFirst(pageNum) || isLast(pageNum) || isNearCurrent) {
+    //         isPageOutOfRange = false;
+    //         return <Pagination.Item key={pageNum} active={pageNum==currentPage} onClick={() => onNumberClick(pageNum)}>
+    //             {pageNum}
+    //         </Pagination.Item>
+    //     }
+    //     if (!isPageOutOfRange) leftEllipsisNumber = pageNum - 1;
+    //
+    //     if (!isPageOutOfRange && (isNextNearCurrent || isNextLast)) {
+    //         isPageOutOfRange = true;
+    //         return <Pagination.Ellipsis key={pageNum} onClick={() => onEllipsisClick(leftEllipsisNumber, pageNum+1)}/>
+    //     }
+    //     return null;
+    // })
 
-    let isPageOutOfRange: boolean = false;
-    let leftEllipsisNumber: number = 1;
-    const pages = [...new Array(pageAmount)].map((_, i) => {
-        const pageNum = i+1;
-        const isNearCurrent = Math.abs(pageNum - currentPage) <= 1;
-        const isNextNearCurrent = Math.abs(pageNum + 1 - currentPage) <= 1;
-        const isNextLast = isLast(pageNum+1);
 
-        if (isFirst(pageNum) || isLast(pageNum) || isNearCurrent) {
-            isPageOutOfRange = false;
-            return <Pagination.Item key={pageNum} active={pageNum==currentPage} onClick={() => onNumberClick(pageNum)}>
-                {pageNum}
-            </Pagination.Item>
+
+
+
+    const numberItem = (num: number) => (
+        <Pagination.Item key={num} active={num==currentPage} onClick={() => onNumberClick(num)}>
+            {num}
+        </Pagination.Item>
+    )
+    const ellipsisItem = (num: number) => (
+        <Pagination.Ellipsis key={num} disabled/>
+    )
+
+    const getRange = (start: number, end: number) => {
+        return Array(end - start + 1)
+            .fill(null)
+            .map((_, i) => numberItem(start+i))
+    }
+
+    const pagination = (currentPage: number, pageCount: number) => {
+        let delta: number
+        if (pageCount <= 7) {
+            // delta == 7: [1 2 3 4 5 6 7]
+            delta = 7
+        } else {
+            // delta == 2: [1 ... 4 5 6 ... 10]
+            // delta == 4: [1 2 3 4 5 ... 10]
+            delta = currentPage > 4 && currentPage < pageCount - 3 ? 2 : 4
         }
-        if (!isPageOutOfRange) leftEllipsisNumber = pageNum - 1;
 
-        if (!isPageOutOfRange && (isNextNearCurrent || isNextLast)) {
-            isPageOutOfRange = true;
-            return <Pagination.Ellipsis key={pageNum} onClick={() => onEllipsisClick(leftEllipsisNumber, pageNum+1)}/>
+        const range = {
+            start: Math.round(currentPage - delta / 2),
+            end: Math.round(currentPage + delta / 2)
         }
-        return null;
-    })
 
-    // const numberItem = (num: number) => (
-    //     <Pagination.Item key={num} active={num==currentPage} onClick={() => onNumberClick(num)}>
-    //         {num}
-    //     </Pagination.Item>
-    // )
-    // const ellipsisItem = (num: number) => (
-    //     <Pagination.Ellipsis key={num}/>
-    // )
-    //
-    // const getRange = (start: number, end: number) => {
-    //     return Array(end - start + 1)
-    //         .fill(null)
-    //         .map((_, i) => numberItem(start+i))
-    // }
+        if (range.start - 1 == 1 || range.end + 1 == pageCount) {
+            range.start += 1
+            range.end += 1
+        }
 
-    // const pagination = (currentPage: number, pageCount: number) => {
-    //     let delta: number
-    //     if (pageCount <= 7) {
-    //         // delta == 7: [1 2 3 4 5 6 7]
-    //         delta = 7
-    //     } else {
-    //         // delta == 2: [1 ... 4 5 6 ... 10]
-    //         // delta == 4: [1 2 3 4 5 ... 10]
-    //         delta = currentPage > 4 && currentPage < pageCount - 3 ? 2 : 4
-    //     }
-    //
-    //     const range = {
-    //         start: Math.round(currentPage - delta / 2),
-    //         end: Math.round(currentPage + delta / 2)
-    //     }
-    //
-    //     if (range.start - 1 == 1 || range.end + 1 == pageCount) {
-    //         range.start += 1
-    //         range.end += 1
-    //     }
-    //
-    //     let pages: Array<ReactNode> =
-    //         currentPage > delta
-    //             ? getRange(Math.min(range.start, pageCount - delta), Math.min(range.end, pageCount))
-    //             : getRange(1, Math.min(pageCount, delta + 1))
-    //
-    //     const withDots = (value: ReactNode, pair: Array<ReactNode>) => (pages.length + 1 != pageCount ? pair : [value])
-    //
-    //     if (pages[0] != 1) {
-    //         pages = withDots(numberItem(1), [numberItem(1), ellipsisItem(2)]).concat(pages)
-    //     }
-    //
-    //     if (pages[pages.length - 1] < pageCount) {
-    //         pages = pages.concat(withDots(numberItem(pageCount), [ellipsisItem(pageCount-1), numberItem(pageCount)]))
-    //     }
-    //
-    //     return pages
-    // }
+        let start = currentPage > delta ? Math.min(range.start, pageCount - delta) : 1;
+        let end =   currentPage > delta ? Math.min(range.end, pageCount) : Math.min(pageCount, delta + 1);
+
+        // let pages: Array<ReactNode> =
+        //     currentPage > delta
+        //         ? getRange(Math.min(range.start, pageCount - delta), Math.min(range.end, pageCount))
+        //         : getRange(1, Math.min(pageCount, delta + 1))
+        let pages: Array<ReactNode> = getRange(start, end);
+
+        const withDots = (value: ReactNode, pair: Array<ReactNode>) => (pages.length + 1 != pageCount ? pair : [value])
+
+        if (start != 1) {
+            pages = withDots(numberItem(1), [numberItem(1), ellipsisItem(2)]).concat(pages)
+        }
+
+        if (end < pageCount) {
+            pages = pages.concat(withDots(numberItem(pageCount), [ellipsisItem(pageCount-1), numberItem(pageCount)]))
+        }
+
+        return pages
+    }
 
     if (isShown) return (
         <Pagination>
-            <Pagination.First onClick={onFirstClick} disabled={isFirstPage}/>
-            <Pagination.Prev onClick={onPrevClick} disabled={isFirstPage}/>
-            {pages}
-            <Pagination.Next onClick={onNextClick} disabled={isLastPage}/>
-            <Pagination.Last onClick={onLastClick} disabled={isLastPage}/>
+            <div className="d-none d-md-flex">
+                <Pagination.First onClick={onFirstClick} disabled={isFirstPage}/>
+                <Pagination.Prev onClick={onPrevClick} disabled={isFirstPage}/>
+            </div>
+            {pagination(currentPage, pageAmount)}
+            <div className="d-none d-md-flex">
+                <Pagination.Next onClick={onNextClick} disabled={isLastPage}/>
+                <Pagination.Last onClick={onLastClick} disabled={isLastPage}/>
+            </div>
         </Pagination>
     )
     return (<></>)
--- src/main/ts/components/misc/TableHeader.tsx
+++ src/main/ts/components/misc/TableHeader.tsx
@@ -16,10 +16,20 @@ export interface TableHeaderProps {
 export const TableHeader: FC<TableHeaderProps> = ({itemsCount, itemsPerPage, setItemsPerPage, currentPage, setCurrentPage, clearPost}) => {
     const {currentData: user} = useGetCurrentUserQuery();
     return (
-        <div className="d-flex justify-content-between">
-            <Button className="h-auto d-inline-block" onClick={() => clearPost(user.id)}>Clear</Button>
-            <PaginationMenuItem itemsCount={itemsCount} itemsPerPage={itemsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-            <DropdownMenuItem itemsPerPage={itemsPerPage} setItemsPerPage={setItemsPerPage}/>
-        </div>
+        <>
+            <div className="justify-content-between d-sm-flex d-md-none">
+                <Button className="h-auto d-inline-block" onClick={() => clearPost(user.id)}>Clear</Button>
+                <DropdownMenuItem itemsPerPage={itemsPerPage} setItemsPerPage={setItemsPerPage}/>
+            </div>
+            <div className="d-flex justify-content-center p-1 justify-content-md-between p-md-0">
+                <div className="d-none d-md-block">
+                    <Button className="h-auto d-inline-block" onClick={() => clearPost(user.id)}>Clear</Button>
+                </div>
+                <PaginationMenuItem itemsCount={itemsCount} itemsPerPage={itemsPerPage} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+                <div className="d-none d-md-block">
+                    <DropdownMenuItem itemsPerPage={itemsPerPage} setItemsPerPage={setItemsPerPage}/>
+                </div>
+            </div>
+        </>
     )
 }
